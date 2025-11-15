890e41fc360b9b2d75df77e279d4da0e1694a100
carosuppo <carosu808@gmail.com>
2025-11-15 13:55:32 -0300

    CRUD de Área ya terminado y probado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReclamoModule } from './reclamo/reclamo.module';
+import { AreaModule } from './area/area.module';
 
 @Module({
-  imports: [],
+  imports: [ReclamoModule, AreaModule],
   controllers: [AppController],
   providers: [AppService],
 })
